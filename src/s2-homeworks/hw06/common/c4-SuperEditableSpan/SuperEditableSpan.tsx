6a27d30aeb23555ea276a67f58884833540d66af
Mger-arzi <mger54598@gmail.com>
2024-01-12 10:03:30 +0300

    add

--- src/s2-homeworks/hw06/common/c4-SuperEditableSpan/SuperEditableSpan.tsx
+++ src/s2-homeworks/hw06/common/c4-SuperEditableSpan/SuperEditableSpan.tsx
@@ -84,8 +84,8 @@ const SuperEditableSpan: React.FC<SuperEditableSpanType> = (
                         {...restSpanProps}
                     >
                         {/*если нет захардкодженного текста для спана, то значение инпута*/}
-
-                        {children || restProps.value || defaultText}
+                        Enter text...
+                        {/* {children || restProps.value || defaultText} */}
                     </span>
                 </div>
             )}
